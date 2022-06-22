2eac3218887d8ce95534edd984a9af20c1610842
ideook <ideook@gmail.com>
2022-06-22 15:46:41 +0900

    window 기능으로 수정

--- src/Table/DataTable.js
+++ src/Table/DataTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import "./DataTable.css";
 import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
 import InputBase from "@mui/material/InputBase";
 import Input from "@mui/material/Input";
 import fileDownload from "js-file-download";
@@ -10,35 +11,86 @@ import axios from "axios";
 const baseuri = "https://ueapi.haeahn.com/api/RvtCollection/";
 
 const columnsFamily = [
-  { field: "SEQ", headerName: "ID", width: 50 },
-  {
-    field: "ID_REL_FILE",
-    headerName: "ID_REL_FILE",
-    width: 120,
-  },
+  { field: "SEQ", headerName: "ID", width: 80 },
+  //{
+  //  field: "ID_REL_FILE",
+  //  headerName: "ID_REL_FILE",
+  //  width: 120,
+  //},
   {
     field: "NM_CATG",
     headerName: "NM_CATG",
-    width: 150,
+    width: 120,
   },
   {
     field: "NM_FML",
     headerName: "NM_FML",
     width: 300,
   },
+  {
+    field: "TotalCount",
+    headerName: "총 유리개수",
+    width: 100,
+  },
+  {
+    field: "ElevCount",
+    headerName: "유리 짝",
+    width: 100,
+  },
+  {
+    field: "HorzCount",
+    headerName: "가로개수",
+    width: 100,
+  },
+  {
+    field: "VertCount",
+    headerName: "세로개수",
+    width: 100,
+  },
+  {
+    field: "IsDouble",
+    headerName: "이중창",
+    width: 100,
+  },
 ];
 
 const columnsSymbol = [
-  { field: "SEQ", headerName: "ID", width: 50 },
+  { field: "SEQ", headerName: "ID", width: 60 },
   {
     field: "ID_REL_FML",
     headerName: "ID_REL_FML",
     width: 120,
+    hide: true,
   },
   {
     field: "NM_SYM",
     headerName: "NM_SYM",
-    width: 150,
+    width: 250,
+  },
+  {
+    field: "IMG_SYM",
+    headerName: "IMG_SYM",
+    width: 300,
+    hide: true,
+  },
+];
+
+const columnsSameFamily = [
+  { field: "SEQ", headerName: "ID", width: 80 },
+  {
+    field: "NM_CATG",
+    headerName: "NM_CATG",
+    width: 120,
+  },
+  {
+    field: "NM_FML",
+    headerName: "NM_FML",
+    width: 300,
+  },
+  {
+    field: "VOL",
+    headerName: "VOL",
+    width: 300,
   },
   {
     field: "IMG_SYM",
@@ -76,15 +128,23 @@ const columnsRelation = [
 
 const DataTable = () => {
   const [dataFamily, setFamily] = useState([]);
+  const [dataSameFamily, setSameFamily] = useState([]);
   const [dataSymbol, setSymbol] = useState([]);
   const [dataSymbolImage, setSymbolImage] = useState("");
+  const [dataSymbolImage2, setSymbolImage2] = useState("");
   const [dataParameter, setParameter] = useState([]);
   const [dataRelation, setRelation] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState("");
   const [headerUserEnter, setHeaderUserEnter] = useState([]);
   const [dataUsgLoad, setDataUsgLoad] = useState([]);
   const [selectionModel, setSelectionModel] = useState([]);
+  const [selectionSameModel, setSelectionSameModel] = useState([]);
   const [snackbar, setSnackbar] = useState(null);
+  const [searchTotal, setSearchTotal] = useState("");
+  const [searchElev, setSearchElev] = useState("");
+  const [searchHorz, setSearchHorz] = useState("");
+  const [searchVert, setSearchVert] = useState("");
+  const [searchDoubleAll, setSearchDoubleAll] = useState(true);
+  const [searchDouble, setSearchDouble] = useState(false);
 
   const handleCloseSnackbar = () => setSnackbar(null);
 
@@ -111,7 +171,7 @@ const DataTable = () => {
 
   const handleGetData = async (e) => {
     const postData = {
-      ID_REL: e.row.SEQ,
+      ID_REL: e.SEQ,
     };
 
     try {
@@ -242,13 +302,53 @@ const DataTable = () => {
     }
   };
 
-  const PreviewImage = async (e) => {
-    //console.log(e.row.IMG_SYM);
+  const handleGetSameVolmne = async (e) => {
+    const postData = {
+      ID: e.SEQ,
+      RATIO: 0.001,
+    };
+
+    try {
+      const res = await fetch(baseuri + "findSameVol", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+      if (!res.ok) {
+        const message = `An error has occured: ${res.status} - ${res.statusText}`;
+        throw new Error(message);
+      }
+      const data = await res.json();
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: {
+          "Content-Type": res.headers.get("Content-Type"),
+          "Content-Length": res.headers.get("Content-Length"),
+        },
+        data: data,
+      };
+      setSameFamily(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handlePreviewImageAndSameData = async (e) => {
+    console.log(e.row);
     setSymbolImage(e.row.IMG_SYM);
+    handleGetData(e.row);
+    handleGetSameVolmne(e.row);
+  };
+
+  const handlePreviewImage = async (e) => {
+    setSymbolImage2(e.row.IMG_SYM);
   };
 
   const handleValueChange = async (e) => {
-    setSearchKeyword(e.target.value);
+    //setSearchKeyword(e.target.value);
     //console.log(e.target.value)
 
     if (e.target.value === "") {
@@ -297,50 +397,230 @@ const DataTable = () => {
     }
   };
 
-  const handleDownload = async () =>{
+  const handleDownload = async () => {
     axios({
       url: baseuri + "filename",
       params: {
-        id:selectionModel[0]
-      }
+        id: selectionModel[0],
+      },
     }).then((response0) => {
       axios({
         url: baseuri + "download",
-        responseType: 'blob',
+        responseType: "blob",
         params: {
-          id:selectionModel[0]
-        }
+          id: selectionModel[0],
+        },
       }).then((response1) => {
         fileDownload(response1.data, response0.data);
       });
-    });    
-  }
+    });
+  };
+
+  const handleChangeTotal = (e) => {
+    if (Number(e.target.value) < 0) return;
+    setSearchTotal(e.target.value);
+  };
+
+  const handleChangeElev = (e) => {
+    if (Number(e.target.value) < 0) return;
+    setSearchElev(e.target.value);
+  };
+
+  const handleChangeHorz = (e) => {
+    if (Number(e.target.value) < 0) return;
+    setSearchHorz(e.target.value);
+  };
+
+  const handleChangeVert = (e) => {
+    if (Number(e.target.value) < 0) return;
+    setSearchVert(e.target.value);
+  };
+
+  const handleChangeDouble = (e) => {
+    setSearchDouble(e.target.checked);
+  };
+
+  const handleChangeDoubleAll = (e) => {
+    setSearchDoubleAll(e.target.checked);
+  };
+
+  const handleClickSearch = () => {
+    handleSearch();
+  };
+
+  const handleSearch = async (e) => {
+    const postData = {
+      TOTAL: searchTotal,
+      ELEV: searchElev,
+      HORZ: searchHorz,
+      VERT: searchVert,
+      DOUBLEALL: searchDoubleAll,
+      DOUBLE: searchDouble,
+    };
+
+    console.log(JSON.stringify(postData));
+
+    try {
+      const res = await fetch(baseuri + "search2", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+      if (!res.ok) {
+        const message = `An error has occured: ${res.status} - ${res.statusText}`;
+        throw new Error(message);
+      }
+      const data = await res.json();
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: {
+          "Content-Type": res.headers.get("Content-Type"),
+          "Content-Length": res.headers.get("Content-Length"),
+        },
+        data: data,
+      };
+      console.log(data);
+
+      // let symbols = [];
+      // for (let i = 0; i < data.length; i++) {
+      //   const d = data[i];
+
+      //   symbols.push({
+      //     SEQ: d.SEQ,
+      //     ID_REL_FML: d.ID_REL_FML,
+      //     NM_SYM: d.NM_SYM,
+      //     IMG_SYM: d.IMG_SYM,
+      //   });
+      // }
+      setFamily(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onRemove = () => {
+    if (selectionSameModel.length === 0) return;
+    if (window.confirm("삭제합니까?")) {
+      handleDelete();
+    }
+  };
+
+  const handleDelete = async () => {
+
+    let families = [];
+    for (let i = 0; i < selectionSameModel.length; i++) {
+      const family = selectionSameModel[i];
+      families.push('' + family);
+    }
+
+    try {
+      const res = await fetch(baseuri + "deleteFamily", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(families),
+      });
+      if (!res.ok) {
+        const message = `An error has occured: ${res.status} - ${res.statusText}`;
+        throw new Error(message);
+      }
+      const data = await res.json();
+      fetchTableFamily();
+      setSameFamily([]);
+      setSymbol([]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div style={{ margin: "10px" }}>
-      <div className="area_menu">
+      <div
+        className="area_menu"
+        style={{
+          width: "100%",
+          margin: "10px 10px 10px 0px",
+          height: "50px",
+          display: "inline-block",
+          verticalAlign: "middle",
+        }}
+      >
         <div
           style={{
-            height: "40px",
+            height: "30px",
             display: "inline-block",
             verticalAlign: "middle",
           }}
         >
           패밀리 브라우저
         </div>
-        <TextField
-          style={{ margin: "0px 10px 10px 20px" }}
-          id="search-text"
-          variant="standard"
-          onChange={handleValueChange}
-          value={searchKeyword}
-        />
-        {/* <button style={{ height: "30px", margin: "5px" }} >
-          검색
-        </button> */}
+        <div>
+          총 유리개수
+          <TextField
+            style={{ margin: "0px 30px 0px 10px", width: "100px" }}
+            id="search-total"
+            type="text"
+            variant="standard"
+            value={searchTotal}
+            onChange={handleChangeTotal}
+          />
+          유리짝 수
+          <TextField
+            style={{ margin: "0px 30px 0px 10px", width: "100px" }}
+            id="search-elev"
+            type="text"
+            variant="standard"
+            value={searchElev}
+            onChange={handleChangeElev}
+          />
+          가로개수
+          <TextField
+            style={{ margin: "0px 30px 0px 10px", width: "100px" }}
+            id="search-horz"
+            type="text"
+            variant="standard"
+            value={searchHorz}
+            onChange={handleChangeHorz}
+          />
+          세로개수
+          <TextField
+            style={{ margin: "0px 30px 0px 10px", width: "100px" }}
+            id="search-vert"
+            type="text"
+            variant="standard"
+            value={searchVert}
+            onChange={handleChangeVert}
+          />
+          이중창 | 전체
+          <Checkbox
+            style={{ margin: "0px 30px 0px 10px" }}
+            id="search-double"
+            value={searchDoubleAll}
+            checked={searchDoubleAll}
+            onChange={handleChangeDoubleAll}
+          />
+          이중창
+          <Checkbox
+            style={{ margin: "0px 30px 0px 10px" }}
+            id="search-double"
+            value={searchDouble}
+            onChange={handleChangeDouble}
+          />
+          <button
+            style={{ height: "30px", margin: "5px", width: "80px" }}
+            onClick={handleClickSearch}
+          >
+            검색
+          </button>
+        </div>
       </div>
       <div style={{ display: "flex", height: "800px" }}>
-        <div style={{ width: "35%", margin: "0px 20px 0px 0px" }}>
+        <div style={{ width: "60%", margin: "0px 20px 0px 0px" }}>
           <div
             style={{
               height: "40px",
@@ -349,21 +629,50 @@ const DataTable = () => {
           >
             Family
           </div>
-            <button style={{ height: "25px", float: "right", margin: "5px" }} onClick={handleDownload}>
-              download
-            </button>
-          <div style={{ height: "100%" }}>
+          <button
+            style={{ height: "25px", float: "right", margin: "5px" }}
+            onClick={handleDownload}
+          >
+            download
+          </button>
+          <div style={{ height: "50%", margin: "0px 0px 20px 0px" }}>
             <DataGrid
               rows={dataFamily}
               columns={columnsFamily}
               getRowId={(row) => row.SEQ}
               rowHeight={35}
               rowsPerPageOptions={[100]}
-              onRowClick={handleGetData}
+              onRowClick={handlePreviewImageAndSameData}
               onSelectionModelChange={(newSelectionModel) => {
                 setSelectionModel(newSelectionModel);
               }}
               selectionModel={selectionModel}
+            />
+          </div>
+          <div style={{ margin: "0px 0px 20px 0px" }}>
+            Equal Volume
+            <button
+              style={{ height: "25px", float: "right", margin: "5px" }}
+              onClick={onRemove}
+            >
+              delete
+            </button>
+          </div>
+
+          <div style={{ height: "45%" }}>
+            <DataGrid
+              rows={dataSameFamily}
+              columns={columnsSameFamily}
+              getRowId={(row) => row.SEQ}
+              rowHeight={35}
+              rowsPerPageOptions={[100]}
+              onRowClick={handlePreviewImage}
+              checkboxSelection
+              //checkboxSelection={checkboxSelection}
+              onSelectionModelChange={(sel) => {
+                setSelectionSameModel(sel);
+              }}
+              selectionModel={selectionSameModel}
             />
           </div>
         </div>
@@ -388,18 +697,18 @@ const DataTable = () => {
                 getRowId={(row) => row.SEQ}
                 rows={dataSymbol}
                 columns={columnsSymbol}
-                onRowClick={PreviewImage}
                 rowHeight={35}
                 pageSize={100}
               />
             </div>
             <div
               style={{
-                height: "40px",
+                height: "370px",
                 display: "inline-block",
+                margin: "0px 20px 0px 0px",
               }}
-            ></div>
-            <div style={{ height: "370px" }}>
+            >
+              <p>Family</p>
               <img
                 src={`data:image/png;base64,${dataSymbolImage}`}
                 width="200"
@@ -407,9 +716,18 @@ const DataTable = () => {
                 alt=""
               />
             </div>
+            <div style={{ height: "370px", display: "inline-block" }}>
+              <p>Same Family</p>
+              <img
+                src={`data:image/png;base64,${dataSymbolImage2}`}
+                width="200"
+                height="200"
+                alt=""
+              />
+            </div>
           </div>
         </div>
-        <div style={{ flexGrow: "1", height: "auto" }}>
+        {/* <div style={{ flexGrow: "1", height: "auto" }}>
           <div style={{ height: "auto" }}>
             <div
               style={{
@@ -446,7 +764,7 @@ const DataTable = () => {
               />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
